c291b11ea579ed8b0e2ac8a70c23b27a1b1e05ae
TheKenetics <TheKenetics@users.noreply.github.com>
2019-06-16 18:26:53 -0500

    Add files via upload
    Add Cylinder

--- __init__.py
+++ __init__.py
@@ -100,6 +100,8 @@ def update_changable_primitive(self, context):
 		bpy.ops.object.cp_ot_update_cube()
 	elif context.active_object.data.changable_primitive_settings.type == "CIRCLE":
 		bpy.ops.object.cp_ot_update_circle()
+	elif context.active_object.data.changable_primitive_settings.type == "CYLINDER":
+		bpy.ops.object.cp_ot_update_cylinder()
 	else:
 		print("You haven't implemented " + context.active_object.data.changable_primitive_settings.type + " in master update yet!")
 
@@ -115,18 +117,20 @@ class CP_changable_primitive_settings(PropertyGroup):
 		name="Enabled",
 		default=False
 	)
+	
+	# TODO: Set icons to correct mesh type
 	type : EnumProperty(
 		items=[
 			("PLANE","Plane","","MESH_PLANE",0),
 			("CUBE","Cube","","MESH_CUBE",1),
 			("CIRCLE","Circle","","MESH_CUBE",2),
-			("CYLINDER","Cylinder","","MESH_CUBE",1),
-			("SPHERE","Sphere","","MESH_CUBE",1),
-			("ICOSPHERE","Icosphere","","MESH_CUBE",1),
-			("TORUS","Torus","","MESH_CUBE",1),
-			("CONE","Cone","","MESH_CUBE",1),
-			("TUBE","Tube","","MESH_CUBE",1),
-			("ARC","Arc","","MESH_CUBE",1),
+			("CYLINDER","Cylinder","","MESH_CUBE",3),
+			("SPHERE","Sphere","","MESH_CUBE",4),
+			("ICOSPHERE","Icosphere","","MESH_CUBE",5),
+			("TORUS","Torus","","MESH_CUBE",6),
+			("CONE","Cone","","MESH_CUBE",7),
+			("TUBE","Tube","","MESH_CUBE",8),
+			("ARC","Arc","","MESH_CUBE",9),
 			],
 		name="Type"
 	)
@@ -134,7 +138,8 @@ class CP_changable_primitive_settings(PropertyGroup):
 	"""
 	Note about subdivision properties:
 	x is also used for circular segments
-	y is also used for circular v subdivisions, ie: the subdivisions going up and down a cylinder
+	y is also used for circular u subdivisions, ie: the subdivisions from inside to outside of a cylinder
+	z is also used for circular v subdivisions, ie: the subdivisions going up and down a cylinder
 	"""
 	x_subdivisions : IntProperty(
 		name="X Subdivisions",
@@ -164,6 +169,35 @@ class CP_changable_primitive_settings(PropertyGroup):
 		name="Cap Type",
 		update=update_changable_primitive
 	)
+	
+	radius : FloatProperty(
+		name="Radius",
+		default=1,
+		update=update_changable_primitive,
+		unit='LENGTH'
+	)
+	
+	diameter1 : FloatProperty(
+		name="Diameter 1",
+		default=1,
+		update=update_changable_primitive,
+		unit='LENGTH'
+	)
+	
+	diameter2 : FloatProperty(
+		name="Diameter 2",
+		default=1,
+		update=update_changable_primitive,
+		unit='LENGTH'
+	)
+	
+	# Also used for plane size, cube size
+	height : FloatProperty(
+		name="Height",
+		default=1,
+		update=update_changable_primitive,
+		unit='LENGTH'
+	)
 
 
 ## Operators
@@ -180,6 +214,12 @@ class CP_OT_create_plane(bpy.types.Operator):
 		size=2,
 		default=(2,2),
 		min=1
+	)
+	
+	size : FloatProperty(
+		name="Size",
+		default=1.0,
+		unit='LENGTH'
 	)
 	
 	align_rot_to_cursor : BoolProperty(
@@ -216,6 +256,7 @@ class CP_OT_create_plane(bpy.types.Operator):
 		settings.x_subdivisions = self.subdivisions[0]
 		settings.y_subdivisions = self.subdivisions[1]
 		settings.z_subdivisions = 2
+		settings.height = self.size
 		
 		# Create mesh
 		bpy.ops.object.cp_ot_update_plane()
@@ -236,13 +277,14 @@ class CP_OT_update_plane(bpy.types.Operator):
 	def execute(self, context):
 		x_subdivisions = context.active_object.data.changable_primitive_settings.x_subdivisions
 		y_subdivisions = context.active_object.data.changable_primitive_settings.y_subdivisions
+		size = context.active_object.data.changable_primitive_settings.height
 		
 		bm = bmesh.new()
 		bm.from_mesh(context.active_object.data)
 		
 		if bm.verts:
 			bmesh.ops.delete(bm, geom=bm.verts, context="VERTS")
-		bmesh.ops.create_grid(bm, x_segments=x_subdivisions, y_segments=y_subdivisions, size=1, calc_uvs=True)
+		bmesh.ops.create_grid(bm, x_segments=x_subdivisions, y_segments=y_subdivisions, size=size, calc_uvs=True)
 		
 		bm.to_mesh(context.active_object.data)
 		bm.free()
@@ -264,6 +306,12 @@ class CP_OT_create_cube(bpy.types.Operator):
 		size=3,
 		default=(2,2,2),
 		min=2
+	)
+	
+	size : FloatProperty(
+		name="Size",
+		default=1.0,
+		unit='LENGTH'
 	)
 	
 	align_rot_to_cursor : BoolProperty(
@@ -301,6 +349,7 @@ class CP_OT_create_cube(bpy.types.Operator):
 		settings.x_subdivisions = self.subdivisions[0]
 		settings.y_subdivisions = self.subdivisions[1]
 		settings.z_subdivisions = self.subdivisions[2]
+		settings.height = self.size
 		
 		# Create mesh
 		bpy.ops.object.cp_ot_update_cube()
@@ -322,6 +371,7 @@ class CP_OT_update_cube(bpy.types.Operator):
 		x_subdivisions = context.active_object.data.changable_primitive_settings.x_subdivisions
 		y_subdivisions = context.active_object.data.changable_primitive_settings.y_subdivisions
 		z_subdivisions = context.active_object.data.changable_primitive_settings.z_subdivisions
+		size = context.active_object.data.changable_primitive_settings.height
 		
 		bm = bmesh.new()
 		bm.from_mesh(context.active_object.data)
@@ -329,7 +379,7 @@ class CP_OT_update_cube(bpy.types.Operator):
 		# Delete old mesh
 		if bm.verts:
 			bmesh.ops.delete(bm, geom=bm.verts, context="VERTS")
-		bmesh.ops.create_cube(bm, size=1, calc_uvs=True)
+		bmesh.ops.create_cube(bm, size=size, calc_uvs=True)
 		
 		# Subdivide X edges
 		if x_subdivisions > 2:
@@ -366,6 +416,18 @@ class CP_OT_create_circle(bpy.types.Operator):
 		name = "Segments",
 		default=32,
 		min=3
+	)
+	
+	u_subdivisions : IntProperty(
+		name = "U Subdivisions",
+		default=2,
+		min=2
+	)
+	
+	radius : FloatProperty(
+		name="Radius",
+		default=1.0,
+		unit='LENGTH'
 	)
 	
 	cap_type : EnumProperty(
@@ -410,7 +472,9 @@ class CP_OT_create_circle(bpy.types.Operator):
 		settings.enabled = True
 		settings.type = "CIRCLE"
 		settings.x_subdivisions = self.segments
+		settings.y_subdivisions = self.u_subdivisions
 		settings.cap_type = self.cap_type
+		settings.radius = self.radius
 		
 		# Create Mesh
 		bpy.ops.object.cp_ot_update_circle()
@@ -430,7 +494,9 @@ class CP_OT_update_circle(bpy.types.Operator):
 
 	def execute(self, context):
 		segments = context.active_object.data.changable_primitive_settings.x_subdivisions
+		u_subdivisions = context.active_object.data.changable_primitive_settings.y_subdivisions
 		cap_type = context.active_object.data.changable_primitive_settings.cap_type
+		radius = context.active_object.data.changable_primitive_settings.radius
 		
 		bm = bmesh.new()
 		bm.from_mesh(context.active_object.data)
@@ -441,20 +507,178 @@ class CP_OT_update_circle(bpy.types.Operator):
 		
 		# Create Circle
 		if cap_type == "NONE":
-			bmesh.ops.create_circle(bm, segments=segments, radius=1, calc_uvs=True)
+			bmesh.ops.create_circle(bm, segments=segments, radius=radius, calc_uvs=True)
 		elif cap_type == "FACE":
-			bmesh.ops.create_circle(bm, segments=segments, radius=1, cap_ends=True, calc_uvs=True)
+			bmesh.ops.create_circle(bm, segments=segments, radius=radius, cap_ends=True, calc_uvs=True)
 		else:
-			bmesh.ops.create_circle(bm, segments=segments, radius=1, cap_ends=True, cap_tris=True, calc_uvs=True)
+			bmesh.ops.create_circle(bm, segments=segments, radius=radius, cap_ends=True, cap_tris=True, calc_uvs=True)
+		
+		if u_subdivisions > 2 and cap_type == "TRI":
+			center_vert = None
 			
+			for vert in bm.verts:
+				if vert.co[0] == 0.0 and vert.co[1] == 0.0:
+					center_vert = vert
+					break
+			
+			bmesh.ops.subdivide_edges(bm, edges=center_vert.link_edges, cuts=u_subdivisions-2)
+		
 		bm.to_mesh(context.active_object.data)
 		bm.free()
 		
 		context.active_object.update_tag()
 		
 		return {'FINISHED'}
+
+
+class CP_OT_create_cylinder(bpy.types.Operator):
+	"""Creates a new Changable Cylinder"""
+	bl_idname = "object.cp_ot_create_cylinder"
+	bl_label = "Create Changable Cylinder"
+	bl_options = {'REGISTER','UNDO'}
 	
+	# Properties
+	segments : IntProperty(
+		name = "Segments",
+		default=32,
+		min=3
+	)
 	
+	u_subdivisions : IntProperty(
+		name = "Segments",
+		default=2,
+		min=2
+	)
+	
+	v_subdivisions : IntProperty(
+		name = "Segments",
+		default=2,
+		min=2
+	)
+	
+	diameter : FloatProperty(
+		name="Diameter",
+		default=1.0,
+		unit='LENGTH'
+	)
+	
+	height : FloatProperty(
+		name="Height",
+		default=1.0,
+		unit='LENGTH'
+	)
+	
+	cap_type : EnumProperty(
+		items=[
+			("NONE","No Cap","","",0),
+			("TRI","Triangle Cap","","",1),
+			("FACE","Face Cap","","",2),
+		],
+		name="Cap Type"
+	)
+	
+	align_rot_to_cursor : BoolProperty(
+		name="Align Rotation to 3D Cursor",
+		default=False
+	)
+
+	@classmethod
+	def poll(cls, context):
+		return True
+
+	def execute(self, context):
+		# Deselect all objects
+		for obj in context.selected_objects:
+			obj.select_set(False)
+		
+		# Create mesh and object
+		obj = create_and_link_mesh_object(context, "ChangableCylinder")
+		obj.location = context.scene.cursor.location.copy()
+		if self.align_rot_to_cursor:
+			obj.rotation_euler = context.scene.cursor.rotation_euler.copy()
+		
+		# Change draw options
+		obj.show_wire = True
+		obj.show_all_edges = True
+		
+		# Set created object as active
+		obj.select_set(True)
+		context.view_layer.objects.active = obj
+		
+		# Initialize Changable Primitive Settings
+		settings = obj.data.changable_primitive_settings
+		settings.enabled = True
+		settings.type = "CYLINDER"
+		settings.x_subdivisions = self.segments
+		settings.y_subdivisions = self.u_subdivisions
+		settings.z_subdivisions = self.v_subdivisions
+		settings.height = self.height
+		settings.diameter1 = self.diameter
+		settings.cap_type = self.cap_type
+		
+		# Create Mesh
+		bpy.ops.object.cp_ot_update_cylinder()
+		
+		return {'FINISHED'}
+
+
+class CP_OT_update_cylinder(bpy.types.Operator):
+	"""Updates a changable cylinder"""
+	bl_idname = "object.cp_ot_update_cylinder"
+	bl_label = "Update Changable Cylinder"
+	bl_options = {'REGISTER', 'UNDO', 'INTERNAL'}
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object.type == "MESH"
+
+	def execute(self, context):
+		segments = context.active_object.data.changable_primitive_settings.x_subdivisions
+		u_subdivisions = context.active_object.data.changable_primitive_settings.y_subdivisions
+		v_subdivisions = context.active_object.data.changable_primitive_settings.z_subdivisions
+		cap_type = context.active_object.data.changable_primitive_settings.cap_type
+		height = context.active_object.data.changable_primitive_settings.height
+		diameter = context.active_object.data.changable_primitive_settings.diameter1
+		
+		bm = bmesh.new()
+		bm.from_mesh(context.active_object.data)
+		
+		# Delete old mesh
+		if bm.verts:
+			bmesh.ops.delete(bm, geom=bm.verts, context="VERTS")
+		
+		# Create Cylinder
+		if cap_type == "NONE":
+			bmesh.ops.create_cone(bm, segments=segments, diameter1=diameter, diameter2=diameter, depth=height, calc_uvs=True)
+		elif cap_type == "FACE":
+			bmesh.ops.create_cone(bm, segments=segments, diameter1=diameter, diameter2=diameter, depth=height, cap_ends=True, calc_uvs=True)
+		else:
+			bmesh.ops.create_cone(bm, segments=segments, diameter1=diameter, diameter2=diameter, depth=height, cap_ends=True, cap_tris=True, calc_uvs=True)
+		
+		# Subdivide U edges
+		if u_subdivisions > 2 and cap_type == "TRI":
+			u_edges = []
+			
+			for vert in bm.verts:
+				if vert.co[0] == 0.0 and vert.co[1] == 0.0:
+					u_edges += vert.link_edges
+					
+			
+			bmesh.ops.subdivide_edges(bm, edges=u_edges, cuts=u_subdivisions-2)
+		
+		# Subdivide V edges
+		if v_subdivisions > 2:
+			v_edges = [edge for edge in bm.edges if edge_verts_distance(edge.verts, 2) > 0]
+			bmesh.ops.subdivide_edges(bm, edges=v_edges, cuts=v_subdivisions-2)
+		
+		bm.to_mesh(context.active_object.data)
+		bm.free()
+		
+		context.active_object.update_tag()
+		
+		return {'FINISHED'}
+
+
 class CP_OT_make_permenant(bpy.types.Operator):
 	"""Makes a Changable Primitive's current shape permanent. (Not able to be updated via UI anymore)"""
 	bl_idname = "object.cp_ot_make_permanent"
@@ -497,6 +721,7 @@ class CP_PT_changable_primitive_settings(bpy.types.Panel):
 			
 			layout.prop(obj.data.changable_primitive_settings, "x_subdivisions")
 			layout.prop(obj.data.changable_primitive_settings, "y_subdivisions")
+			layout.prop(obj.data.changable_primitive_settings, "height", text="Size")
 			layout.operator(CP_OT_make_permenant.bl_idname)
 		elif obj.data.changable_primitive_settings.type == "CUBE":
 			layout.label(text="Cube", icon="MESH_PLANE")
@@ -504,12 +729,25 @@ class CP_PT_changable_primitive_settings(bpy.types.Panel):
 			layout.prop(obj.data.changable_primitive_settings, "x_subdivisions")
 			layout.prop(obj.data.changable_primitive_settings, "y_subdivisions")
 			layout.prop(obj.data.changable_primitive_settings, "z_subdivisions")
+			layout.prop(obj.data.changable_primitive_settings, "height", text="Size")
 			layout.operator(CP_OT_make_permenant.bl_idname)
 		elif obj.data.changable_primitive_settings.type == "CIRCLE":
 			layout.label(text="Circle", icon="MESH_PLANE")
 			
 			layout.prop(obj.data.changable_primitive_settings, "x_subdivisions", text="Segments")
+			layout.prop(obj.data.changable_primitive_settings, "y_subdivisions", text="U Subdivisions")
 			layout.prop(obj.data.changable_primitive_settings, "cap_type")
+			layout.prop(obj.data.changable_primitive_settings, "radius")
+			layout.operator(CP_OT_make_permenant.bl_idname)
+		elif obj.data.changable_primitive_settings.type == "CYLINDER":
+			layout.label(text="Cylinder", icon="MESH_PLANE")
+			
+			layout.prop(obj.data.changable_primitive_settings, "x_subdivisions", text="Segments")
+			layout.prop(obj.data.changable_primitive_settings, "y_subdivisions", text="U Subdivisions")
+			layout.prop(obj.data.changable_primitive_settings, "z_subdivisions", text="V Subdivisions")
+			layout.prop(obj.data.changable_primitive_settings, "cap_type")
+			layout.prop(obj.data.changable_primitive_settings, "diameter1", text="Diameter")
+			layout.prop(obj.data.changable_primitive_settings, "height")
 			layout.operator(CP_OT_make_permenant.bl_idname)
 		else:
 			layout.label(text="This one hasn't been implemented in Panel yet! " + obj.data.changable_primitive_settings.type)
@@ -519,10 +757,12 @@ class CP_MT_changable_primitives_base(Menu):
 
 	def draw(self, _context):
 		layout = self.layout
-
-		layout.operator(CP_OT_create_plane.bl_idname)
-		layout.operator(CP_OT_create_cube.bl_idname)
-		layout.operator(CP_OT_create_circle.bl_idname)
+		
+		# TODO: Set icons to right mesh type
+		layout.operator(CP_OT_create_plane.bl_idname, text="Plane")
+		layout.operator(CP_OT_create_cube.bl_idname, text="Cube")
+		layout.operator(CP_OT_create_circle.bl_idname, text="Circle")
+		layout.operator(CP_OT_create_cylinder.bl_idname, text="Cylinder")
 
 
 ## Append to UI Functions
@@ -540,6 +780,8 @@ classes = (
 	CP_OT_update_cube,
 	CP_OT_create_circle,
 	CP_OT_update_circle,
+	CP_OT_create_cylinder,
+	CP_OT_update_cylinder,
 	CP_OT_make_permenant,
 	CP_PT_changable_primitive_settings,
 	CP_MT_changable_primitives_base
